63613ae391d04fe060253b4152182d5853ed6dec
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-08-20 15:54:45 -0500

    alter help text

--- bot/help.rb
+++ bot/help.rb
@@ -1,7 +1,6 @@
 class Help < SlackRubyBot::Commands::Base
    command 'help' do |client, data, match|
-      options = "Here are some commands you can tell me:  \n view all \n view <order number> \n create order <name>, <date of lunch>, <restaurant> \n add to <order name>, <your name>, <item>, <notes>"
-      result =
+      options = "*Here are some commands you can tell me:*  \n view all - list of all orders in this channel \n view <order number> - list order details and items \n create order <name>, <date of lunch>, <restaurant> \n add to <order name>, <your name>, <item>, <notes>"
       client.say(channel: data.channel, text: options)
    end
  end
