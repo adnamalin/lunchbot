db675e9178ddb44cb93a5a73240fd4fbfea7ccd9
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-08-22 11:02:10 -0500

    delete order works

---
+++ bot/delete_order.rb
@@ -0,0 +1,11 @@
+class DeleteOrder < SlackRubyBot::Commands::Base
+   command 'delete order' do |client, data, match|
+      order = Order.order_by_channel(data.channel).where(id: match['expression'])
+      if order != []
+        order.first.delete
+        client.say(channel: data.channel, text: "Order #{order.first.id} has been deleted")
+      else
+        client.say(channel: data.channel, text: "Sorry, I could not find that order in this channel.")
+      end
+   end
+ end
