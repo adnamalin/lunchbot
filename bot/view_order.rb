ee7ad2ad640fda7a801de0333e8a0e5a131ad6eb
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-08-22 10:46:41 -0500

    adjust display

--- bot/view_order.rb
+++ bot/view_order.rb
@@ -1,3 +1,5 @@
+require 'json'
+
 class ViewOrder < SlackRubyBot::Commands::Base
    command 'view' do |client, data, match|
     #data: <Slack::Messages::Message channel="G2382L9AQ" team="T1CEKUERG" text="<@U236XH7L6> view all" ts="1471722466.000022" type="message" user="U1CE4LJ76">
@@ -14,7 +16,10 @@ class ViewOrder < SlackRubyBot::Commands::Base
       if order != []
         client.say(channel: data.channel, text: order.first.print_single_order)
       else
-        client.say(channel: data.channel, text: "Sorry, I could not find that order in this channel.")
+        # TODO: how to send attachments? it's sending voer in json but the client is not parsing it to writing
+        p "-----------SENDING-------------"
+         client.say(channel: data.channel, text: "Sorry, I could not find that order in this channel.", attachments: [{"title": "The Further Adventures of Slackbot","author_name": "Stanford S. Strickland"}])
+        p "-----------SENDING-------------"
       end
     end
    end
