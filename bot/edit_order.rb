41a86bc775cbc8653cae6198fe114be67ec0e9fd
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-08-22 18:55:24 -0500

    make conditional for edits

--- bot/edit_order.rb
+++ bot/edit_order.rb
@@ -1,13 +1,24 @@
 class EditOrder < SlackRubyBot::Commands::Base
    command 'edit order' do |client, data, match|
      #edit order 1, date, next thursday 5:00
-     #need better way to split, user has to write it in very specific way
       order_details = match['expression'].split(", ")
+      order_num = order_details[0]
+      order_attr = order_details[1]
+      order_change = order_details[2]
       order = Order.order_by_channel(data.channel).where(id: order_num)
-      # TODO: make adjustment for if their changing date
-      # TODO: make adjustments for link to clean up < >
       if order != []
-        order.update("#{order_attr}": order_change)
+        case order_attr
+          when "date"
+            Time.zone = "UTC"
+            Chronic.time_class = Time.zone
+            date_cleaned = Chronic.parse(order_change)
+            order.update("lunch_date": date_cleaned)
+          when "menu"
+            link_cleaned = order_change.gsub!(/[<>]/, '')
+            order.update("#{order_attr}": link_cleaned)
+          else
+            order.update("#{order_attr}": order_change)
+        end
         client.say(channel: data.channel, text: "I have updated order #{order_num}'s #{order_attr} to #{order_change}")
       else
         client.say(channel: data.channel, text: "Sorry, I could not find that order in this channel.")
