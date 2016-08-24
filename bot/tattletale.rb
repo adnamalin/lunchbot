100f266e66bfda7c186c9b0a5ebaa2e547cc1b6b
AMANDA LIN <AmandaLin@AMANDAs-MacBook-Air.local>
2016-08-23 22:13:57 -0500

    require delete item

--- bot/tattletale.rb
+++ bot/tattletale.rb
@@ -1,11 +1,12 @@
 $:.unshift File.dirname(__FILE__)
- require 'help'
- require 'channel_join'
+require 'help'
+require 'channel_join'
 require 'view_order'
 require 'create_order'
 require 'delete_order'
 require 'edit_order'
 require 'add_item'
+require 'delete_item'
 
  Thread.abort_on_exception = true
 
